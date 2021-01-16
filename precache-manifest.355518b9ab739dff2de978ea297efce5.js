4a5b3d458dcdb1fad00e170a96bdf58becd101bc
AnnaPalii <annapalii@yahoo.com>
2021-01-16 16:37:19 -0500

    Updates

---
+++ precache-manifest.355518b9ab739dff2de978ea297efce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942408be396ede53235c6e62c8e3326b",
+    "url": "/annapaliiport/index.html"
+  },
+  {
+    "revision": "3ae7423a22d8097f0db0",
+    "url": "/annapaliiport/static/css/main.754bd2a0.chunk.css"
+  },
+  {
+    "revision": "27c36c797b44045cd605",
+    "url": "/annapaliiport/static/js/2.e24377c3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/annapaliiport/static/js/2.e24377c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae7423a22d8097f0db0",
+    "url": "/annapaliiport/static/js/main.b8c40945.chunk.js"
+  },
+  {
+    "revision": "f5459061a5cf4d1dc7b3",
+    "url": "/annapaliiport/static/js/runtime-main.77bc1643.js"
+  }
+]);
