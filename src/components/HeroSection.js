1c62ac4d1204e601f29d04dd1f33f6d0584610d5
AnnaPalii <annapalii@yahoo.com>
2021-01-16 16:46:10 -0500

    cchanges to video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import Video from '../videos/video-3.mov'
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-3.mov' autoPlay loop muted />
+      <video src={Video} autoPlay loop muted />
       <h1>Hi! I am Anna.</h1>
     </div>
   );
